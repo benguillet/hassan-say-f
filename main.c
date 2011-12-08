08d052cc8668006301b8a7a927778469630b3961
Ludovic Henry <ludovichenry.utbm@gmail.com>
2011-12-08 17:32:54 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: ludovic
+ *
+ * Created on 8 décembre 2011, 17:38
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
